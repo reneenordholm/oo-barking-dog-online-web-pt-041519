854459ef684180210bbe212d56eda80dffedcd9c
Renee Nordholm <reneenordholm@gmail.com>
2019-05-08 20:37:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,11 +8,14 @@ class Dog
     @this_dogs_name
   end
   
+  def bark
+    puts "Woof!"
+  end
+  
 end
  
 fido = Dog.new
 fido.name = "Fido"
 puts fido.name
 
-fido.bark = "woof!"
 puts fido.bark
